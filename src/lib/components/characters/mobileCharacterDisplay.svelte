5d94f178b139f3f22ec0e672484ef1cf70c76266
Alexander Allman <alexallmandev@gmail.com>
2026-07-01 12:25:56 +0100

    First pass of mobile character screen with corner gradient although image overpowers it by being in foreground

--- src/lib/components/characters/mobileCharacterDisplay.svelte
+++ src/lib/components/characters/mobileCharacterDisplay.svelte
@@ -1,20 +1,76 @@
 <script lang="ts">
 	
-	let {image, name} = $props();
+	let {image, name, corner} = $props();
+	let hovering = $state(false);
 </script>
 
-<div>
-	<img src={image} alt="{name} xel" />
+<div style="--corner-colour: {corner};" class="container"
+     onpointerenter={() => hovering = true}
+     onpointerleave={() => hovering = false}>
+	<div 
+		class={[hovering && "hoverEnter", !hovering && "hoverExit", "img-container"]}
+	>
+		<img src={image} alt="{name} xel" />
+	</div>
 	<h1>{name}</h1>
 </div>
 
 <style>
-	div {
+	.container {
 			display: flex;
 			flex-direction: column;
-			margin: 5vh 6vw;
+			margin: 3vh 6vw;
       box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.19);
       border: lightslategrey solid 2px;
       border-radius: 10px;
+			height: 25vh;
 	}
+	
+	.img-container {
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(145deg, transparent 70%, var(--corner-colour) 20%);
+	}
+	
+	img {
+      width: 100%;
+      height: 100%;
+			object-fit: cover;
+	}
+	
+	h1 {
+			position: relative;
+			bottom: 35%;
+			right: 3%;
+			text-align: right;
+	}
+
+  @keyframes hoverAnimEnter {
+      from {
+          background: linear-gradient(145deg, transparent 70%, var(--corner-colour) 20%);
+      }
+
+      to {
+          background: linear-gradient(145deg, transparent 40%, var(--corner-colour) 20%);
+      }
+  }
+
+  @keyframes hoverAnimExit {
+      from {
+          background: linear-gradient(145deg, transparent 40%, var(--corner-colour) 20%);
+      }
+
+      to {
+          background: linear-gradient(145deg, transparent 70%, var(--corner-colour) 20%);
+      }
+  }
+
+  .hoverEnter{
+      animation: hoverAnimEnter linear 3s;
+  }
+
+  .hoverExit{
+      animation: hoverAnimExit linear 3s;
+  }
+	
 </style>
--- src/routes/characters/+page.svelte
+++ src/routes/characters/+page.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
 	import {isMobile} from '$lib/windowEvents';
 	import MobileCharacterDisplay from '$lib/components/characters/mobileCharacterDisplay.svelte';
+	import hrothXel from '$lib/assets/logo.png';
+	import bearXel from '$lib/assets/bearXel.png';
 
 	const tempCharacters = [
-		{"name": "Bear", "image": "bx-code"},
-		{"name": "Hroth", "image": "bx-book-open"},
+		{"name": "Bear", "image": bearXel, "corner": "#e8bc8d"},
+		{"name": "Hroth", "image": hrothXel, "corner": "#00aaff"},
+		{"name": "Charr", "image": "bx-book-open", "corner": "#cc6655"},
 	]
 </script>
 
 {#if $isMobile}
-	<h1> Mobile View</h1>
 	{#each tempCharacters as char}
-		<MobileCharacterDisplay name={char.name} image={char.image} />
+		<MobileCharacterDisplay name={char.name} image={char.image} corner={char.corner} />
 	{/each}
 {:else}
 	<h1> Desktop View</h1>
