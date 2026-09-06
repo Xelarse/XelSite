c7fae0a203dc9688139bbe92e03413a567e0e800
Alex Allman <alexallmandev@gmail.com>
2026-09-06 16:03:20 +0100

    Adjusted character display to handle mobile formatting, fixed gradient view on mobile form

---
+++ src/lib/components/characters/characterDisplay.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import {isMobile} from '$lib/windowEvents';
+	
+	let {image, name, corner} = $props();
+	let hovering = $state(false);
+</script>
+
+
+{#if $isMobile}
+		<div style="--corner-colour: {corner}; --gradient-length: {hovering ? "40%" : "70%"}; --gradient-pitch: {hovering ? "180deg" : "145deg"};"
+				 class="container"
+				 role="link"
+				 tabindex="0"
+				 onpointerenter={() => hovering = true}
+				 onpointerleave={() => hovering = false}>
+			<img src={image} alt="{name} xel" />
+			<div class="banner-gradient"></div>
+			<h1>{name}</h1>
+
+		</div>
+	{:else}
+	<div></div>
+{/if}
+
+<style>
+	.container {
+			display: flex;
+			flex-direction: column;
+			margin: 3vh 6vw;
+      box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.19);
+      border: lightslategrey solid 2px;
+      border-radius: 10px;
+			height: 25vh;
+			position: relative;
+	}
+
+	.banner-gradient {
+			width: 100%;
+			height: 100%;
+			position: absolute;
+      background: linear-gradient(var(--gradient-pitch), transparent var(--gradient-length), var(--corner-colour) 80%);
+	}
+	
+	img {
+      width: 100%;
+      height: 100%;
+			object-fit: cover;
+			position: absolute;
+	}
+	
+	h1 {
+			position: absolute;
+			bottom: -5%;
+			right: 5%;
+			text-align: right;
+	}
+</style>
--- src/routes/characters/+page.svelte
+++ src/routes/characters/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import {isMobile} from '$lib/windowEvents';
-	import MobileCharacterDisplay from '$lib/components/characters/mobileCharacterDisplay.svelte';
+	import CharacterDisplay from '$lib/components/characters/characterDisplay.svelte';
 	import hrothXel from '$lib/assets/logo.png';
 	import bearXel from '$lib/assets/bearXel.png';
 
@@ -8,16 +7,14 @@
 		{"name": "Bear", "image": bearXel, "corner": "#e8bc8d"},
 		{"name": "Hroth", "image": hrothXel, "corner": "#00aaff"},
 		{"name": "Charr", "image": "bx-book-open", "corner": "#cc6655"},
+		{"name": "Charr", "image": "bx-book-open", "corner": "#cc6655"},
+		{"name": "Charr", "image": "bx-book-open", "corner": "#cc6655"},
 	]
 </script>
 
-{#if $isMobile}
-	{#each tempCharacters as char}
-		<MobileCharacterDisplay name={char.name} image={char.image} corner={char.corner} />
-	{/each}
-{:else}
-	<h1> Desktop View</h1>
-{/if}
+{#each tempCharacters as char}
+	<CharacterDisplay name={char.name} image={char.image} corner={char.corner} />
+{/each}
 
 <style>
 	
